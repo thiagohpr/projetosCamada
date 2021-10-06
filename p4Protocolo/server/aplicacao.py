0aa9bc28e8d535e92fccc439a5c136b8632e2b69
thiagohpr <62914339+thiagohpr@users.noreply.github.com>
2021-10-06 20:51:45 -0300

    Update aplicacao.py

--- p4Protocolo/server/aplicacao.py
+++ p4Protocolo/server/aplicacao.py
@@ -46,6 +46,29 @@ def cria_head(tipo,n_atual,n_total,n_bytespay):
     head[9]=(170).to_bytes(1, byteorder='big')
     return head
 
+def cria_log(formato,head):
+    
+    tipo=str(int.from_bytes(head[0], byteorder='big'))
+    tamanho_total=str(int.from_bytes(head[5], byteorder='big')+14)
+    linha=time.ctime()+" / "+formato+" / "+tipo+" / "+tamanho_total
+    if tipo == '3':
+        crc=(hex(int.from_bytes(head[8], byteorder='big'))[2:]+hex(int.from_bytes(head[9], byteorder='big'))[2:]).upper()
+        linha+=" / "+str(int.from_bytes(head[4], byteorder='big'))+" / "+str(int.from_bytes(head[3], byteorder='big'))+" / "+crc
+    return linha
+
+def log_envio(head):
+    with open("C:/Users/thpro/Desktop/Camada Física/projetosCamada/p4Protocolo/server/log.txt", "a") as file:
+        file.write(cria_log("envio",head))
+        file.write('\n')
+def log_recebeu(head):
+    with open("C:/Users/thpro/Desktop/Camada Física/projetosCamada/p4Protocolo/server/log.txt", "a") as file:
+        file.write(cria_log("receb",head))
+        file.write('\n')
+def cria_lista(binario):
+    lista=[]
+    for bit in binario:
+        lista.append((bit).to_bytes(1, byteorder='big'))
+    return lista
 eop1=b'\xaa'
 eop2=b'\xff'
 eop_fixo=[eop2,eop1,eop2,eop1]
@@ -58,88 +81,127 @@ def main():
         
         # Ativa comunicacao. Inicia os threads e a comunicação seiral 
         com1.enable()
-        print("comunicacao aberta")
-
+        with open("C:/Users/thpro/Desktop/Camada Física/projetosCamada/p4Protocolo/server/log.txt", "w") as file:
+            file.write("Comunicação aberta")
+            file.write('\n')
+        print("Comunicacao aberta")
+        encerrar=False
         ocioso = True
-
+        startTimer2 = int(time.time())
         while ocioso:
             rxBuffer, nRx = com1.getData(14)
             if rxBuffer[0] == 1:
                 print("mensagem certa")
                 if rxBuffer[2]==meu_id:
+                    log_recebeu(cria_lista(rxBuffer))
                     print("é pra mim")
                     ocioso = False
-            time.sleep(1)
-
-        numPckg = rxBuffer[3]
-        print("numero certo")
-        cont = 1
-        msgTipo2=cria_head(2,cont,numPckg,0)
+                    numPckg = rxBuffer[3]
+                    print("numero certo")
+                    cont = 1
+                    cont_anterior=0
+                    msgTipo2=cria_head(2,cont,numPckg,0)
         
-        msgTipo2=msgTipo2+eop_fixo
-        print(msgTipo2)
-        com1.sendData(np.asarray(msgTipo2))
+                    msgTipo2=msgTipo2+eop_fixo
+                    print(msgTipo2)
+                    com1.sendData(np.asarray(msgTipo2))
+                    log_envio(msgTipo2)
+            else:
+                time_now = int(time.time())
+                if time_now >= startTimer2 + 20:
+                    ocioso = True
+                    msgTipo5=cria_head(5,0,0,0)
+                    msgTipo5=msgTipo5+eop_fixo 
+                    com1.sendData(np.asarray(msgTipo5))
+                    log_envio(msgTipo5)
+                    encerrar=True
+            if encerrar==True:
+                break
+            time.sleep(1)
+        
 
-        arquivo=[0]*numPckg
-        while cont <= numPckg:
-            recebeu = False
-            seconds_to_go_for = 20
-            timer2 = int(time.time())
+        if encerrar==False:
+            arquivo=[0]*numPckg
+            while cont <= numPckg:
+                recebeu = False
+                seconds_to_go_for = 20
+                timer2 = int(time.time())
 
-            while not recebeu:
-                head, headn = com1.getData(10)
-                print(head)
-                payload, payloadn = com1.getData(head[5])
-                print(payload)
-                eop, eopn = com1.getData(4)
-                print(eop)
-                if head[0] == 3:
-                    recebeu = True
-                    print("mensagem certa")
-                    print (head[7])
-                    if head[4] == head[7] + 1:
-                        if len(payload) == head[5]:
-                            arquivo[cont-1] = payload
+                while not recebeu:
+                    if encerrar==False:
+                        head, headn = com1.getData(10)
+                        print(head)
+                        if len (head)>1:
+                            payload, payloadn = com1.getData(head[5])
+                            print(payload)
+                            eop, eopn = com1.getData(4)
+                            print(eop)
+                        if head[0] == 3:
+                            recebeu = True
+                            log_recebeu(cria_lista(head))
+                            print("mensagem certa")
+                            print (head[7])
+                            if head[4] == head[7] + 1:
+                                if head[4]==cont_anterior+1:
+                                    if len(payload) == head[5]:
+                                        arquivo[cont-1] = payload
+                                        cont_anterior=cont
+                                        msgTipo4=cria_head(4,cont,numPckg,0)
+                                        msgTipo4=msgTipo4+eop_fixo
 
-                            msgTipo4=cria_head(4,cont,numPckg,0)
-                            msgTipo4=msgTipo4+eop_fixo
-
-                            com1.sendData(np.asarray(msgTipo4))
-                            cont+=1
-                            print("enviando confirmação")
-                            print(cont)
-                            print(numPckg)
+                                        com1.sendData(np.asarray(msgTipo4))
+                                        log_envio(msgTipo4)
+                                        cont+=1
+                                        print("enviando confirmação")
+                                        print(cont)
+                                        print(numPckg)
+                                    else:
+                                        msgTipo6=cria_head(6,cont,numPckg,0)
+                                        msgTipo6=msgTipo6+eop_fixo 
+                                        com1.sendData(np.asarray(msgTipo6))
+                                        log_envio(msgTipo6)
+                                        print("erro na quantidade de bytes")
+                                else:
+                                    msgTipo6=cria_head(6,cont,numPckg,0)
+                                    msgTipo6=msgTipo6+eop_fixo 
+                                    com1.sendData(np.asarray(msgTipo6))
+                                    log_envio(msgTipo6)
+                                    print("erro número do pacote")
+                            else:
+                                msgTipo6=cria_head(6,cont,numPckg,0)
+                                msgTipo6=msgTipo6+eop_fixo 
+                                com1.sendData(np.asarray(msgTipo6))
+                                log_envio(msgTipo6)
+                                print("erro número do pacote")
                         else:
-                            msgTipo6=cria_head(6,cont,numPckg,0)
-                            msgTipo6=msgTipo6+eop_fixo 
-                            com1.sendData(np.asarray(msgTipo6))
-                            print("erro na quantidade de bytes")
+                            time.sleep(1)
+
+                            time_now = int (time.time())
+                            if time_now >= timer2 + seconds_to_go_for:
+                                print("Timer 2 encerrado. Timeout de envio do arquivo, finalizando a comunicação.")
+                                ocioso = True
+
+                                msgTipo5=cria_head(5,cont,numPckg,0)
+                                msgTipo5=msgTipo5+eop_fixo 
+
+                                com1.sendData(np.asarray(msgTipo5))
+                                log_envio(msgTipo5)
+                                encerrar=True
+
+                            else:
+                                if head == (255).to_bytes(1, byteorder='big'):
+                                    msgTipo4=cria_head(4,cont,numPckg,0)
+                                    msgTipo4=msgTipo4+eop_fixo 
+                                    com1.sendData(np.asarray(msgTipo4))
+                                    log_envio(msgTipo4)
                     else:
-                        msgTipo6=cria_head(6,cont,numPckg,0)
-                        msgTipo6=msgTipo6+eop_fixo 
-                        com1.sendData(np.asarray(msgTipo6))
-                        print("erro número do pacote")
-                else:
-                    time.sleep(1)
-
-                    time_now = int (time.time())
-                    if time_now >= timer2 + seconds_to_go_for:
-                        ocioso = True
-
-                        msgTipo5=cria_head(5,cont,numPckg,0)
-                        msgTipo5=msgTipo5+eop_fixo 
-
-                        com1.sendData(np.asarray(msgTipo5))
-                        com1.disable()
-
-                    else:
-                        if head == (255).to_bytes(1, byteorder='big'):
-                            msgTipo4=cria_head(4,cont,numPckg,0)
-                            msgTipo4=msgTipo4+eop_fixo 
-                            com1.sendData(np.asarray(msgTipo4))
-
+                        break
+                if encerrar==True:
+                    break
 
         com1.disable()
+        with open("C:/Users/thpro/Desktop/Camada Física/projetosCamada/p4Protocolo/server/log.txt", "a") as file:
+            file.write("Comunicação encerrada")
 
     except Exception as erro:
         print("ops! :-\\")
